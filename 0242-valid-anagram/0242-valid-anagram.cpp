8bf68c681a79c9400da7d26c9046a0069310f5a1
Shaik Khasim Saheb <shaikkhasimsaheb18@gmail.com>
2026-08-01 21:59:56 +0530

    Time: 0 ms (100%), Space: 9.8 MB (50.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
-        }
+        
+ if (s.length() != t.length())
+            return false;
+vector<int>frq1(26,0);
+vector<int>frq2(26,0);
+
+
+for(int i=0;i<s.length();i++){
+    frq2[t[i]-'a']++;
+     frq1[s[i]-'a']++;
+}
+
+
+       for (int i = 0; i < 26; i++) {
+    if (frq1[i] != frq2[i])
         return false;
+}
+return true;
     }
 };
