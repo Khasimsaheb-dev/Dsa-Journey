fe1535c81455750eb6195fc33530f8fe8055a44f
Shaik Khasim Saheb <shaikkhasimsaheb18@gmail.com>
2026-08-06 07:22:48 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.74%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int count=0;
+        int maxi=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' ){
+                count++;
+                maxi=max(maxi,count);
+            }
+            else if(s[i]==')'){
+                count--;
+            }
+        }
+        return maxi;
+    }
+};
