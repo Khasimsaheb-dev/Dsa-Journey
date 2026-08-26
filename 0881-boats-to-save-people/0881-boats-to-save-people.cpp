1572e26357032f33ff37312bbc9cde0b175a26b7
Shaik Khasim Saheb <shaikkhasimsaheb18@gmail.com>
2026-08-26 23:06:31 +0530

    Time: 19 ms (48.23%), Space: 45.7 MB (71.3%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n=people.size();
+        sort(people.begin(),people.end());
+        int left=0;
+        int right=n-1;
+        int count=0;
+        while(left<=right){
+            int sum=people[left]+people[right];
+            if(sum<=limit){
+                left++;
+                right--;
+            }
+            else{
+                right--;
+            }
+            count++;
+        }
+        return count;
+    }
+};
